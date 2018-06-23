b9b097a0a14216cca240583f63de6f0241fc9428
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-06-23 14:04:49 -0400

    complete!

--- mindex-js-code-challenge/src/app/employee/employee.component.ts
+++ mindex-js-code-challenge/src/app/employee/employee.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 //Bring in Employee components for properties from employee.ts (firstName,lastName, position, compensation)
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
+import { directiveCreate } from '@angular/core/src/render3/instructions';
 
 
 @Component({
@@ -13,6 +14,7 @@ import { EmployeeService } from '../employee.service';
 export class EmployeeComponent implements OnInit {
   @Input() employee: Employee;
   @Input() allReports: number;
+  public total: number = 0;
 
   constructor(private employeeService: EmployeeService) {
   }
@@ -22,14 +24,40 @@ export class EmployeeComponent implements OnInit {
     //start with calling the function to grab the all of the employees
     this.getAllReports();
   }
-
-  //function grab all the employees and push them to an array
+  //function to grab all the employees and push them to an array
   getAllReports(): void {
     //put into an array as done in employee-list component
-    let employeeList: Employee[] = [];
-    this.employeeService.getAll().subscribe(){
-      //Need to push indirect employees to array to display on page
-    
+    let emp = this.employee;
+
+    // console.log(emp);
+    // console.log("works");
+
+    //Need to check if each employee has a direct report. If they don't use return to leave function
+    if (!emp.directReports) {
+      return
     }
+    console.log(emp)
+    //query the get request by id
+
+    //for infinite children need to recusively get indirect reports using a function
+    let getIndirectReports = (id) => {
+      this.employeeService.get(id).subscribe(childEmp => {
+        if (!childEmp.directReports) {
+          return
+        }
+        //Need to get length of array for indirect employees
+        this.total += childEmp.directReports.length
+        childEmp.directReports.forEach(id => {
+          getIndirectReports(id);
+        })
+        console.log(childEmp);
+      })
+    }
+
+    emp.directReports.forEach(id => {
+      //Going through all direct reports of person we are mapping to get their indirect reports
+      getIndirectReports(id);
+      this.total++
+    })
   }
 }
